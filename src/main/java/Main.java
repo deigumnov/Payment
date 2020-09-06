3681fbdf6e220de67ee4c50aba8991a8b0d5e12d
Dmitrii <deigumnov@gmail.com>
2020-09-06 23:28:19 +1000

    Добавлена структура работы программы

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void read () {
+        String phone = "79991234567";
+        double sum = 100.00;
+
+
+    }
+
+    public static void check () {
+
+    }
+
+    public static void pay () {
+
+    }
+
+    public static void show () {
+
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        Client client = new Client();
+
+        read();
+        check();
+        pay();
+        show();
+    }
+}
