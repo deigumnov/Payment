0b1565f4b480d7b72aef8fec8f938e4421b0c843
Dmitrii <deigumnov@gmail.com>
2020-10-21 23:05:30 +1000

    Change JAVA version to 11

--- src/main/java/Common/Check.java
+++ src/main/java/Common/Check.java
@@ -5,13 +5,10 @@ public class Check {
     // необходимо вводить только цифры вместо символов X
     // ввод дополнительных символов кроме X считается некорректным, первый символ должен быть равен 8
     public static <T> boolean checkPhone(T inputPhone) {
-        boolean result = true;
+        boolean result = false;
         String phone = inputPhone.toString();
-        if (!phone.startsWith("8")) {
-            result = false;
-        }
-        if (phone.length() != 11) {
-            result = false;
+        if ((phone.startsWith("8") && phone.length() == 11) || (phone.startsWith("+8") && phone.length() == 12)) {
+            result = true;
         }
         return result;
     }
